9640afb1099e37f47cc08e70a890e6a1b7650fbf
Francesco <francesco.0297@hotmail.it>
2023-06-21 16:57:37 +0200

    Fixed Function randomCentroidGenerator Fixed Function getLineNumber

--- src/main/java/it/unipi/dii/cc/hadoop/Centroid.java
+++ src/main/java/it/unipi/dii/cc/hadoop/Centroid.java
@@ -1,16 +1,20 @@
 package it.unipi.dii.cc.hadoop;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.StringTokenizer;
 
 public class Centroid extends Point {
-  private IntWritable id;
+  private IntWritable id; // FRA: Attributo da lasciare?
 
   Centroid() {
     super();
@@ -24,11 +28,16 @@ public class Centroid extends Point {
     this.id = new IntWritable(-1);
   }
 
-  // Create a deep copy of coordinate
   Centroid(IntWritable id, List<DoubleWritable> coordinates) {
     super(coordinates);
 
     this.id = new IntWritable(id.get());
+  }
+
+  Centroid(String coordinates, int configurationDimension)
+  {
+    super(coordinates, configurationDimension);
+    this.id = new IntWritable(-1);
   }
 
   public IntWritable getId() {
@@ -96,20 +105,72 @@ public class Centroid extends Point {
     }
   }
 
-  public void calculateMean(long numElements) {
+  public void calculateMean(long numElements)
+  {
     int length = this.getCoordinates().size();
     
-    for(int i = 0; i < length; i++){
+    for(int i = 0; i < length; i++)
+    {
       Double centroidCoordinate = this.getCoordinates().get(i).get();
       Double mean = centroidCoordinate / numElements;
-      //
-      mean = (double) Math.round(mean * 1000000d) / 1000000d;
-      //
+
+      mean = (double) Math.round(mean * 1000000d) / 1000000d; // FRA: Frare prova con e senza questa approssimazione
+
       this.getCoordinates().set(i, new DoubleWritable(mean));
     }
   }
 
+  public static List<Centroid> randomCentroidGenerator( String INPUT_FILE, String k, String DIM, String OUTPUT_FILE, Configuration conf) throws IOException
+  {
+    final int numCentroid = Integer.parseInt(k);
+    final int dimension = Integer.parseInt(DIM);
+    final List<Centroid> randomCentroidsList = new ArrayList<>();
 
+    Random random = new Random();
+    List<Integer> indexRandomCentroid = new ArrayList<>(); // Lista contenente 'numCentroid' numeri casuali univoci
+
+    int pick;
+    int dataSetSize = getLineNumber(INPUT_FILE, conf);
+
+    while(indexRandomCentroid.size() < numCentroid)
+    {
+      pick = random.nextInt(dataSetSize) + 1;
+      if(!indexRandomCentroid.contains(pick))
+        indexRandomCentroid.add(pick);
+    }
+
+    Path path = new Path(INPUT_FILE);
+    FileSystem hdfs = FileSystem.get(conf);
+
+    int currentLine = 0;
+    String line;
+
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(hdfs.open(path))))
+    {
+      while( ((line = reader.readLine()) != null) && (randomCentroidsList.size() < numCentroid) )
+      {
+        if (indexRandomCentroid.contains(currentLine))
+          randomCentroidsList.add(new Centroid(line, dimension));
+        currentLine++;
+      }
+    }
+    return randomCentroidsList;
+}
+
+  private static int getLineNumber(String INPUT_FILE, Configuration conf) throws IOException
+  {
+    FileSystem fs = FileSystem.get(conf);
+    Path path = new Path(INPUT_FILE);
+
+    int count = 0;
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(fs.open(path)))) {
+      while (reader.readLine() != null) {
+        count++;
+      }
+    }
+    fs.close();
+    return count;
+  }
   
 }
 
