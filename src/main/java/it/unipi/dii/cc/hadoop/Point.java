eeab371c9174abe5662fa357077e6db7e47fbf69
Francesco <francesco.0297@hotmail.it>
2023-06-27 18:16:43 +0200

    added comments for project delivery added compareTo function

--- src/main/java/it/unipi/dii/cc/hadoop/Point.java
+++ src/main/java/it/unipi/dii/cc/hadoop/Point.java
@@ -5,48 +5,54 @@ import java.io.DataOutput;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
 
 import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableComparable;
 
-public class Point implements WritableComparable<Centroid>
+public class Point implements WritableComparable<Point>
 {
   private List<DoubleWritable> coordinates;
 
   Point() {
-    this.coordinates = new ArrayList<DoubleWritable>();
+    this.coordinates = new ArrayList<>();
   }
   Point(final int n)
   {
-    this.coordinates = new ArrayList<DoubleWritable>();
+    this.coordinates = new ArrayList<>();
 
     for (int i = 0; i < n; i++)
       this.coordinates.add(new DoubleWritable(0.0));
   }
 
-  // Declare a new list and then we add to the list every coordinates
+  // Create a new Point with the passed coordinates as List
   Point(final List<DoubleWritable> coordinatesList)
   {
-    this.coordinates = new ArrayList<DoubleWritable>();
+    this.coordinates = new ArrayList<>();
 
     for (final DoubleWritable element : coordinatesList)
         this.coordinates.add(new DoubleWritable(element.get()));
   }
 
+  // Create a new Point with the passed coordinates as splitted String by ','
   Point (String coordinatesReceived, int configurationDimension)
   {
-    this.coordinates = new ArrayList<DoubleWritable>();
+    this.coordinates = new ArrayList<>();
+    String[] splittedCoordinates = coordinatesReceived.split(",");
+    for(int i = 0; (i < splittedCoordinates.length) && (splittedCoordinates.length < configurationDimension); i++)
+    {
+      coordinates.add(new DoubleWritable(Double.parseDouble(splittedCoordinates[i])));
+    }
+
+    /* DA ELIMINARE
     Text word = new Text();
     StringTokenizer itr = new StringTokenizer(coordinatesReceived, ",");
 
     while (itr.hasMoreTokens() && (coordinates.size() < configurationDimension))
     {
       word.set(itr.nextToken());
-      Double coordinate = Double.valueOf(word.toString());
+      double coordinate = Double.parseDouble(word.toString());
       coordinates.add(new DoubleWritable(coordinate));
-    }
+    }*/
   }
 
   @Override
@@ -62,12 +68,30 @@ public class Point implements WritableComparable<Centroid>
   public void readFields(final DataInput in) throws IOException
   {
     final int size = in.readInt();
-    this.coordinates = new ArrayList<DoubleWritable>();
+    this.coordinates = new ArrayList<>();
 
     for (int i = 0; i < size; i++)
         this.coordinates.add(new DoubleWritable(in.readDouble()));
   }
 
+  @Override
+  public int compareTo(Point o) // Checks if 'this' and 'o' are the same point. Returns 0 if equals, else -1
+  {
+    if(this.coordinates.size() != o.getCoordinates().size())
+      return -1;
+
+    int notEqual = 0;
+
+    for(int i = 0; i < this.coordinates.size(); i++)
+    {
+      if(Double.compare(o.getCoordinates().get(i).get(), this.getCoordinates().get(i).get()) != 0)
+        notEqual = 1;
+    }
+
+    return (notEqual);
+  }
+
+  // Conversion of a Point object in a Comma Separated String
   @Override
   public String toString()
   {
@@ -81,17 +105,5 @@ public class Point implements WritableComparable<Centroid>
     return elements;
   }
 
-  @Override
-  public int compareTo(Centroid o) {    return 0;   }
-
   List<DoubleWritable> getCoordinates() {   return this.coordinates;    }
-
-  void setCoordinates(final List<DoubleWritable> newCoordinates)
-  {
-    this.coordinates = new ArrayList<DoubleWritable>();
-
-    for (final DoubleWritable value : newCoordinates)
-        this.coordinates.add(new DoubleWritable(value.get()));
-  }
-
 }
